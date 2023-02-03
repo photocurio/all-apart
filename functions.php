80e67c9d2812367c5b4c9061c6775d4000469a2d
photocurio <sunraydust@gmail.com>
2023-02-03 07:44:06 -0500

    Bring prism into vendor folder, add theme version to enqueues

--- functions.php
+++ functions.php
@@ -6,45 +6,94 @@
  */
 
 add_action( 'wp_enqueue_scripts', 'all_apart_enqueue_styles' );
-function all_apart_enqueue_styles() {
+add_action( 'wp_head', 'all_apart_add_favicon' );
+add_action( 'login_head', 'all_apart_add_favicon' );
+add_action( 'admin_head', 'all_apart_add_favicon' );
+add_filter( 'chaplin_color_schemes', 'all_apart_colors' );
 
-	$parent_style = 'chaplin_style';
+/**
+ * Adding the these stylesheet, and prism js.
+ *
+ * @return void
+ */
+function all_apart_enqueue_styles(): void {
+	$theme_version = wp_get_theme( 'all apart' )->get( 'Version' );
+	$parent_style  = 'chaplin_style';
 
-	wp_register_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_register_style(
+		$parent_style,
+		get_template_directory_uri() . '/style.css',
+		null,
+		$theme_version
+	);
 	wp_enqueue_style(
 		'child-style',
 		get_stylesheet_directory_uri() . '/style.css',
 		array( $parent_style ),
-		wp_get_theme()->get( 'Version' )
+		$theme_version
 	);
-	wp_enqueue_script( 'prism-js', 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/components/prism-core.min.js', null );
-	wp_enqueue_script( 'prism-autoloader', 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/plugins/autoloader/prism-autoloader.min.js' );
-	wp_enqueue_script( 'theme-script', get_stylesheet_directory_uri() . '/theme-script.js', array( 'prism-js', 'prism-autoloader' ) );
-	wp_enqueue_style( 'prism-css', 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/themes/prism.min.css', array( 'child-style' ) );
+	wp_enqueue_style(
+		'prism-css',
+		get_stylesheet_directory_uri() . '/vendor/prism.min.css',
+		array( 'child-style' ),
+		$theme_version
+	);
+
+	wp_enqueue_script(
+		'prism-js',
+		get_stylesheet_directory_uri() . '/vendor/prism-core.min.js',
+		null,
+		$theme_version,
+		true
+	);
+	wp_enqueue_script(
+		'prism-autoloader',
+		get_stylesheet_directory_uri() . '/vendor/prism-autoloader.min.js',
+		null,
+		$theme_version,
+		true
+	);
+	wp_enqueue_script(
+		'theme-script',
+		get_stylesheet_directory_uri() . '/theme-script.js',
+		array( 'prism-js', 'prism-autoloader' ),
+		$theme_version,
+		true
+	);
 }
 
-if ( ! function_exists( 'all_apart_body_classes' ) ) {
-	function all_apart_body_classes( $classes ) {
-		if ( is_category() || is_home() ) {
-			$classes[] = 'overlay-header';
-		}
-		return $classes;
+/**
+ * Add class overlay-header to body.
+ *
+ * @param mixed $classes The body classes.
+ * @return mixed
+ */
+function all_apart_body_classes( $classes ) {
+	if ( is_category() || is_home() ) {
+		$classes[] = 'overlay-header';
 	}
-	add_action( 'body_class', 'all_apart_body_classes' );
+	return $classes;
 }
+add_action( 'body_class', 'all_apart_body_classes' );
 
-function add_favicon() {
+
+/**
+ * Add favicon link.
+ *
+ * @return void
+ */
+function all_apart_add_favicon() {
 	$favicon_url = get_stylesheet_directory_uri() . '/favicon.ico';
-	echo "<link rel='shortcut icon' href='{$favicon_url}'/>";
+	echo "<link rel='shortcut icon' href='" . esc_url( $favicon_url ) . "'/>";
 }
 
-add_action( 'wp_head', 'add_favicon' );
-add_action( 'login_head', 'add_favicon' );
-add_action( 'admin_head', 'add_favicon' );
-
-add_filter( 'chaplin_color_schemes', 'all_apart_colors' );
-
-function all_apart_colors( $array ) {
+/**
+ * Set color definitions.
+ *
+ * @param array $array the array of colors for the theme.
+ * @return array
+ */
+function all_apart_colors( array $array ): array {
 	$array['all-apart'] = array(
 		'name'   => _x( 'All Apart', 'Color scheme name', 'chaplin' ),
 		'colors' => array(
